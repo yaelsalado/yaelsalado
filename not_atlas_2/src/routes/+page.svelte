5e680de70c833be34700c2efee1b6eedc42b2c2a
yaelsalado <a01637736@tec.mx>
2026-02-24 17:29:04 -0600

    agregada naa con framework

---
+++ not_atlas_2/src/routes/+page.svelte
@@ -0,0 +1,229 @@
+<h1>
+    About
+</h1>
+
+<br>
+
+<div class="content">
+
+    <h2>
+        Who is kollektiv orangotango? 
+    </h2>
+
+    <p>
+        kollektiv orangotango was founded in 2008. Since then it has been constantly developing through a network of critical geographers, friends and activists who deal with questions regarding space, power and re­sistance. With our geographical activism, we seek to support processes and oppositional actors who instigate social change by prefiguring social alternatives. We conduct emancipatory educational work as well as con­crete political and artistic interventions. These are supposed to enforce reflections on and changes of social conditions. Through workshops, publications, mappings, excursions, and creative in­terventions within public space, we collectively learn how to read space and how to initiate emancipatory processes from below. By sticking to the traditions of activist research, we connect theoret­ical reflections and concrete actions.
+    </p>
+
+    <br>
+
+    <p>
+        So far we have engaged in the fields of right to the city, (urban) agriculture, critical pedagogy, alternative housing and solidarity economy, mostly in Europe and also in Latin America. But kollektiv orangotango also functions as a platform for dif­ferent actions. In the case of Not-an-Atlas, its publication was realized by kollektiv orangotango in cooperation with other activists and academics. That is the reason why it was named kollektiv orangotango+.
+    </p>
+
+    <br>
+
+    <p>
+        All of this is happen­ing through collective and self-organized practice. In other words: “We’re going slow, because we’re going far”!
+    </p>
+
+    <br>
+
+    <p id="contact">
+        If you want to support us to spread This Is Not an Atlas please get in contact with us.
+        For updates please follow us on <a href="https://de-de.facebook.com/not.an.atlas/" target="_blank"> facebook</a>  or check our blog notanatlas.org/blog.
+        If you have any questions, suggestions or critique, please write us at contact@notanatlas.org
+    </p>
+
+    <h2>
+        How to Edit and Reflect Counter-Cartographies Collectively as Activists and Academics?
+    </h2>
+
+    <p>
+        Both our activist experiences with collective and critical mapping workshops as kollektiv orangotango as well as our individual and academic engagements with maps served as starting points for Not-an-Atlas. In the spring of 2015 we released a Call for Maps in English, German and Spanish, inviting everyone engaged with practices of critical mapping to send us ideas, texts, photos and, of course, maps. We were overwhelmed by the resonance! Nearly 150 submissions found their way to us. Some of them came from places, struggles and activists we had not heard of before; oth­ers came from long-time companions and friends. Among them were contributions from well-known critical cartographers, ac­tivists, and social movements who have been working with maps for many years; but even more emerging mapmakers sent us their proposals. What followed were exciting three years of discussing, selecting and editing the various maps and projects. Again and again we had to ask ourselves: What actually makes a map (not) critical? And what do we (not) have in mind when working on a counter-atlas? We also had to continually negotiate between more activist and more academic modes of knowledge production – be it in regards to the content of the maps or in regards to our own mode of working and living. With the large number of submis­sions, Not-an-Atlas quickly outgrew its initial frame in terms of involved languages, time and resources. The resulting process of collective learning therefore included various challenges, personal fluctuations and surprises. There we started to think about an online version of Not-an-Atlas. So we understand notanatlas.org as a continuation of the Not-an-Atlas book. Because we want to continue to share maps, struggles, projects, texts and inspirations online.
+    </p>
+
+    <br>
+
+    <div class="images">
+        <img 
+            src="/imagenes/imagen_1.jpeg" 
+            alt="img1"
+            width="350"
+            height="300"
+        />
+
+        <img 
+            src="/imagenes/imagen_2.jpg" 
+            alt="img2"
+            width="350"
+            height="300"
+        />
+
+        <img 
+        src="/imagenes/imagen_3.jpeg" 
+        alt="img3"
+        width="350"
+        height="300"
+        />
+
+    </div>
+    <br>
+
+
+    <div class="social_media">
+        <a href="https://de-de.facebook.com/not.an.atlas/" target="_blank">
+            <img 
+                src="/imagenes/facebook-icon.svg"  
+                alt="Facebook" 
+                width="25" 
+                height="25"
+            />
+        </a>
+    </div>
+
+</div>
+
+<div class=footer>
+
+    <div class="madeby">
+        <h3>
+            Realized by
+        </h3>
+
+        <img 
+            src="/imagenes/ORANGO_LOGO.png" 
+            alt="logo"
+            width="600"
+            height="100"
+        />
+
+    </div>
+
+    <div class="social">
+        
+        <h3>
+            Find us on
+        </h3>
+        <a href="https://de-de.facebook.com/not.an.atlas/" target="_blank">
+            <img 
+                src="/imagenes/facebook-icon.svg"  
+                alt="Facebook" 
+                width="50" 
+                height="50"
+            />
+        </a>
+
+    </div>
+
+    <div class="newsletter">
+        <h3>
+            Newsletter
+        </h3>
+
+
+        <form action="https://notanatlas.us1.list-manage.com/subscribe/post?u=8c9e5b0a7f2c8d9e4b1a1c6e&amp;id=0b2f5c8e7b" method="post" target="_blank" novalidate>
+            <input type="email" name="EMAIL" placeholder="email" required>
+            <button type="submit">Subscribe</button>
+        </form>
+
+    </div>
+
+    <br><br>
+    <h1>HECHO CON SVELTE Y TAILWIND</h1>
+
+</div>
+
+
+
+
+
+<style>
+    h1{
+        margin:0;
+        font-weight: normal;
+        text-align: center;
+        padding: 20px;
+    }
+
+    .content {
+        margin: 10px;
+        padding: 50px;
+        line-height: 2;
+    }
+
+    #contact {
+        max-width: 700px;
+    }
+
+    .images {
+        display: flex;
+        justify-content: space-around;
+        margin-top: 50px;
+    }
+
+    .social_media {
+        display: flex;
+        justify-content: center;
+    }
+
+    .footer {
+        margin-top: 50px;
+        background-color: #e0e0e0; 
+        padding: 40px 20px;
+    }
+
+    .madeby {
+        margin-top: 20px;
+    }
+
+    .madeby img{
+        display: block;
+        margin: 0 auto;
+        
+    }
+
+    .social {
+        margin-top: 20px;
+        justify-content: left;
+        display: flex;
+    }
+
+    .social img {
+        margin-left: 40px;
+    }
+
+    .newsletter form {
+    display: flex;
+    gap: 10px;
+    margin-top: 10px;
+    }
+
+    .newsletter input[type="email"] {
+        height: 50px;
+        width: 220px; /* menos ancho */
+        padding: 0 12px;
+        font-size: 16px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+    }
+
+ 
+    .newsletter button {
+        height: 50px;
+        padding: 0 20px;
+        background-color: #007BFF; /* azul */
+        color: white;
+        border: none;
+        border-radius: 4px;
+        font-size: 16px;
+        cursor: pointer;
+        transition: all 0.3s ease;
+    }
+
+    .newsletter button:hover {
+        background-color: white;
+        color: #007BFF;
+        border: 1px solid #007BFF;
+    }
+
+</style>
