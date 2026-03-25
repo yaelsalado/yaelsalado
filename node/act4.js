75893b3a8230d75aea917bad43bfab013b93ef9d
Yael Arturo Salado Soto <yael@MacBook-Pro-de-Yael-2.local>
2026-03-24 18:31:06 -0600

    act4

---
+++ node/act4.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+app.get("/nombre", async (req, res) => {
+    const nombre = req.query.nombre;
+
+    if (!nombre) {
+        res.send("no me diste un nombre de artista");
+        return;
+    }
+    
+    const respuesta = await fetch(`https://www.theaudiodb.com/api/v1/json/123/search.php?s=${nombre}`);
+    const data = await respuesta.json();
+    res.json(data);
+
+});
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo en puerto 3000");
+});
